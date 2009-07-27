7b7a196385767cf4b492c681accaaee75e3fc88d
Francisco Souza <franciscosouza@live.com>
2009-07-27 00:21:38 -0300

    Alteração do modelo.

--- cursos/models.py
+++ cursos/models.py
@@ -1,10 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Instrutor(models.Model):
-    nome = models.CharField(max_length = 100)
+    nome = models.CharField(max_length = 200)
+    descricao = models.TextField()
+    slug = models.SlugField(max_length = 100, blank = True, unique = True)
 
 class Curso(models.Model):
     titulo = models.CharField(max_length = 300)
+    descricao = models.TextField()
     carga_horaria = models.IntegerField()
     instrutor = models.ForeignKey(Instrutor)
+    slug = models.SlugField(max_length = 100, blank = True, unique = True)
+
+class Aluno(User):
+    cpf = models.CharField(max_length = 11)
+
+class Telefone(models.Model):
+    ddd = models.CharField(max_length = 2)
+    numero = models.CharField(max_length = 8)
+    proprietario = models.ForeignKey(Aluno)
+
+class Inscricao(models.Model):
+    status = models.BooleanField()
+    aluno = models.ForeignKey(Aluno)
+    curso = models.ForeignKey(Curso)
