75c868105520d458da338e081e6bd13029491db9
Francisco Souza <franciscossouza@gmail.com>
2009-07-27 00:10:33 -0300

    Definição inicial do módulo models.py da aplicação cursos.

--- cursos/models.py
+++ cursos/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Instrutor(models.Model):
+    nome = models.CharField(max_length = 100)
+
+class Curso(models.Model):
+    titulo = models.CharField(max_length = 300)
+    carga_horaria = models.IntegerField()
+    instrutor = models.ForeignKey(Instrutor)
