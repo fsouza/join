998523257bdd4210725bc23561a39c0a688837ec
Francisco Souza <franciscosouza@live.com>
2009-07-27 20:12:51 -0300

    Trabalho para construção das views.

--- cursos/models.py
+++ cursos/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.urlresolvers import reverse
 from django.contrib.auth.models import User
 
 # Create your models here.
@@ -6,6 +7,12 @@ class Instrutor(models.Model):
     nome = models.CharField(max_length = 200)
     descricao = models.TextField()
     slug = models.SlugField(max_length = 100, blank = True, unique = True)
+
+    def get_url(self):
+        return reverse('cursos.views.instrutor', kwargs = { 'slug' : self.slug })
+
+    def __unicode__(self):
+        return self.nome
 
 class Curso(models.Model):
     titulo = models.CharField(max_length = 300)
@@ -15,6 +22,12 @@ class Curso(models.Model):
     pre_requisitos = models.TextField()
     slug = models.SlugField(max_length = 100, blank = True, unique = True)
 
+    def get_url(self):
+        return reverse('cursos.views.curso', kwargs = { 'slug' : self.slug })
+
+    def __unicode__(self):
+        return self.titulo
+
 class Material(models.Model):
     arquivo = models.FileField(upload_to = 'cursos/materiais')
     curso = models.ForeignKey('Curso')
@@ -22,10 +35,16 @@ class Material(models.Model):
 class Aluno(User):
     cpf = models.CharField(max_length = 11)
 
+    def __unicode__(self):
+        return self.username
+
 class Telefone(models.Model):
     ddd = models.CharField(max_length = 2)
     numero = models.CharField(max_length = 8)
     proprietario = models.ForeignKey('Aluno')
+
+    def __unicode__(self):
+        return '(%s) %s' % (self.ddd, self.numero)
 
 class Inscricao(models.Model):
     status = models.BooleanField()
--- cursos/views.py
+++ cursos/views.py
@@ -1,1 +1,18 @@
 # Create your views here.
+from django.shortcuts import get_object_or_404
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+from models import Instrutor
+from models import Curso
+
+def instrutor(request, slug):
+    instrutor = get_object_or_404(Instrutor, slug = slug)
+    return render_to_response('', locals(), context_instance = RequestContext(request))
+
+def curso(request, slug):
+    curso = get_object_or_404(Curso, slug = slug)
+    return render_to_response('', locals(), context_instance = RequestContext(request))
+
+def cursos(request):
+    cursos = Curso.objects.all()
+    return render_to_response('', locals(), context_instance = RequestContext(request))
