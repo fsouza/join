15b62febe45c14af472736c134d7a53a8dedd360
Francisco Souza <franciscosouza@live.com>
2009-07-27 18:55:39 -0300

    Alteração no modelo.

--- cursos/models.py
+++ cursos/models.py
@@ -11,8 +11,13 @@ class Curso(models.Model):
     titulo = models.CharField(max_length = 300)
     descricao = models.TextField()
     carga_horaria = models.IntegerField()
-    instrutor = models.ForeignKey(Instrutor)
+    instrutor = models.ForeignKey('Instrutor')
+    pre_requisitos = models.TextField()
     slug = models.SlugField(max_length = 100, blank = True, unique = True)
+
+class Material(models.Model):
+    arquivo = models.FileField(upload_to = 'cursos/conteudos')
+    curso = models.ForeignKey('Curso')
 
 class Aluno(User):
     cpf = models.CharField(max_length = 11)
@@ -20,9 +25,9 @@ class Aluno(User):
 class Telefone(models.Model):
     ddd = models.CharField(max_length = 2)
     numero = models.CharField(max_length = 8)
-    proprietario = models.ForeignKey(Aluno)
+    proprietario = models.ForeignKey('Aluno')
 
 class Inscricao(models.Model):
     status = models.BooleanField()
-    aluno = models.ForeignKey(Aluno)
-    curso = models.ForeignKey(Curso)
+    aluno = models.ForeignKey('Aluno')
+    curso = models.ForeignKey('Curso')
