736ea366c8d72bb1bef57fd28302cf26279c8aa5
Francisco Souza <franciscosouza@live.com>
2009-07-27 22:07:38 -0300

    Personalização para as páginas de administração.

--- cursos/admin.py
+++ cursos/admin.py
@@ -1,5 +1,15 @@
 from django.contrib import admin
+from django.contrib.admin.options import ModelAdmin
 from models import *
+
+class CursoAdmin(ModelAdmin):
+    list_display = ('titulo', 'carga_horaria',)
+    search_fields = ('titulo', 'descricao',)
+    list_filter = ('instrutor',)
+
+class InstrutorAdmin(ModelAdmin):
+     list_display = ('nome',)
+     search_fields = ('nome',)
 
 admin.site.register(Aluno)
 admin.site.register(Instrutor)
