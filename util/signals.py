04a1da35c82ffad1a51dabc5d48e277b16c1dfcd
Francisco Souza <franciscosouza@live.com>
2009-07-27 23:13:28 -0300

    Ajust no nome de uma variável no módulo signals.py.

--- util/signals.py
+++ util/signals.py
@@ -5,11 +5,11 @@ def instrutor_pre_save(signal, instance, sender, **kwargs):
     if not instance.slug:
         slug = slugify(instance.nome)
         novo_slug = slug
-        contador = 0
+        count = 0
 
         while sender.objects.filter(slug = novo_slug).exclude(id = instance.id).count() > 0:
-            contador += 1
-            novo_slug = '%s-%d' % (slug, contador)
+            count += 1
+            novo_slug = '%s-%d' % (slug, count)
 
         instance.slug = novo_slug
 
@@ -17,10 +17,10 @@ def curso_pre_save(signal, instance, sender, **kwargs):
     if not instance.slug:
         slug = slugify(instance.titulo)
         novo_slug = slug
-        contador = 0
+        count = 0
 
         while sender.objects.filter(slug = novo_slug).exclude(id = instance.id).count() > 0:
-            contador += 1
-            novo_slug = '%s-%d' % (slug, contador)
+            count += 1
+            novo_slug = '%s-%d' % (slug, count)
 
         instance.slug = novo_slug
