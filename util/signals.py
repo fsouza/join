af5952e36e2e89a2ca61c984545192c44680f99c
Francisco Souza <franciscosouza@live.com>
2009-08-09 14:05:06 -0300

    Alteração no módulo signals.py

--- util/signals.py
+++ util/signals.py
@@ -4,23 +4,23 @@ from django.template.defaultfilters import slugify
 def instrutor_pre_save(signal, instance, sender, **kwargs):
     if not instance.slug:
         slug = slugify(instance.nome)
-        novo_slug = slug
+        new_slug = slug
         count = 0
 
-        while sender.objects.filter(slug = novo_slug).exclude(id = instance.id).count() > 0:
+        while sender.objects.filter(slug = new_slug).exclude(id = instance.id).count() > 0:
             count += 1
-            novo_slug = '%s-%d' % (slug, count)
+            new_slug = '%s-%d' % (slug, count)
 
-        instance.slug = novo_slug
+        instance.slug = new_slug
 
 def curso_pre_save(signal, instance, sender, **kwargs):
     if not instance.slug:
         slug = slugify(instance.titulo)
-        novo_slug = slug
+        new_slug = slug
         count = 0
 
-        while sender.objects.filter(slug = novo_slug).exclude(id = instance.id).count() > 0:
+        while sender.objects.filter(slug = new_slug).exclude(id = instance.id).count() > 0:
             count += 1
-            novo_slug = '%s-%d' % (slug, count)
+            new_slug = '%s-%d' % (slug, count)
 
-        instance.slug = novo_slug
+        instance.slug = new_slug
