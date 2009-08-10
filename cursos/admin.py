28c0c69dc11edf161f8c07f3ec8dbf4ce4a10b8d
Francisco Souza <franciscosouza@live.com>
2009-08-10 15:27:16 -0300

    Alteração na foto do instrutor.
    Agora a foto do instrutor é salva em miniatura e armazena o código do mesmo.

--- cursos/admin.py
+++ cursos/admin.py
@@ -1,3 +1,8 @@
+try:
+    import Image
+except ImportError:
+    from PIL import Image
+
 from django.contrib import admin
 from django.contrib.admin.options import ModelAdmin
 from models import Aluno, Instrutor, Inscricao, Telefone, Curso
@@ -10,6 +15,18 @@ class CursoAdmin(ModelAdmin):
 class InstrutorAdmin(ModelAdmin):
      list_display = ('nome',)
      search_fields = ('nome',)
+
+     def save_model(self, request, obj, form, change):
+         super(InstrutorAdmin, self).save_model(request, obj, form, change)
+         if 'foto' in form.changed_data:
+             extensao = obj.foto.name.split('.')[-1]
+
+             nova_foto = Image.open(obj.foto.path)
+             nova_foto.thumbnail((100, 100), Image.ANTIALIAS)
+
+             obj.foto = 'cursos/fotos_instrutores/%d.%s' % (obj.id, extensao)
+             nova_foto.save(obj.foto.path)
+             obj.save()
 
 admin.site.register(Aluno)
 admin.site.register(Instrutor, InstrutorAdmin)
