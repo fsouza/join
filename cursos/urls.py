d6d3820716caf5f50ab258b982bdc4bc51bf51bc
Francisco Souza <franciscosouza@live.com>
2009-08-10 22:30:58 -0300

    Ajuste no mapemento da URL de visualização dos detalhes de um instrutor.

--- cursos/urls.py
+++ cursos/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls.defaults import *
 urlpatterns = patterns('cursos.views',
             url(r'^$', 'cursos'),
             url(r'^(?P<slug>[\w_-]+)/$', 'curso'),
-            url(r'^instrutores/(?P<slug>[\w_-]+)$', 'instrutor'),
+            url(r'^instrutor/(?P<slug>[\w_-]+)$', 'instrutor'),
             url(r'^inscricao/(?P<slug>[\w_-]+)$', 'inscricao'),
             url(r'^alunos/cadastro/$', 'cadastro'),
             url(r'^alunos/cadastrar/$', 'cadastrar'),
