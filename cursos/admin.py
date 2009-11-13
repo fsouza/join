9bb58c046ca5cc1f393b7a743dd42e24ff5e3f42
Francisco Souza <francisco@franciscosouza.net>
2009-11-12 22:26:57 -0200

    Ajuste de alguns warnings apontados pelo Pydev.

--- cursos/admin.py
+++ cursos/admin.py
@@ -13,20 +13,19 @@ class CursoAdmin(ModelAdmin):
     list_filter = ('instrutor',)
 
 class InstrutorAdmin(ModelAdmin):
-     list_display = ('nome',)
-     search_fields = ('nome',)
+    list_display = ('nome',)
+    search_fields = ('nome',)
 
-     def save_model(self, request, obj, form, change):
-         super(InstrutorAdmin, self).save_model(request, obj, form, change)
-         if 'foto' in form.changed_data:
-             extensao = obj.foto.name.split('.')[-1]
+    def save_model(self, request, obj, form, change):
+        super(InstrutorAdmin, self).save_model(request, obj, form, change)
+        if 'foto' in form.changed_data:
+            extensao = obj.foto.name.split('.')[-1]
+            nova_foto = Image.open(obj.foto.path)
+            nova_foto.thumbnail((100, 100), Image.ANTIALIAS)
 
-             nova_foto = Image.open(obj.foto.path)
-             nova_foto.thumbnail((100, 100), Image.ANTIALIAS)
-
-             obj.foto = 'cursos/fotos_instrutores/%d.%s' % (obj.id, extensao)
-             nova_foto.save(obj.foto.path)
-             obj.save()
+            obj.foto = 'cursos/fotos_instrutores/%d.%s' % (obj.id, extensao)
+            nova_foto.save(obj.foto.path)
+            obj.save()
 
 admin.site.register(Aluno)
 admin.site.register(Instrutor, InstrutorAdmin)
--- cursos/forms.py
+++ cursos/forms.py
@@ -1,10 +1,8 @@
 #coding:utf-8
 
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-
-from models import Aluno, Curso
+from models import Aluno
 from util.validacoes import validar_cpf
 
 class AlunoForm(forms.ModelForm):
